eb3c7765214965334f6b41ce16e8d6cd5a7319a0
vkings1 <elmer.alluad@gmail.com>
2020-04-12 15:10:18 +0800

    webrtc video/audio

---
+++ README.md
@@ -0,0 +1,25 @@
+# WebRTC Video/Audio Broadcast
+
+Sends a video from the broadcast page to all connected clients (watchers) over WebRTC PeerToPeer connections.
+
+## Getting started
+
+Start using Node
+
+```bash
+# Install dependencies for server
+npm install
+
+# Run the server
+node server
+```
+
+
+
+## Author
+
+Elmer T Alluad Jr
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE) file for details
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const server = require('http').createServer(app);
+const io = require("socket.io").listen(server);
+
+let broadcaster;
+
+server.listen(process.env.PORT || 3300);
+
+app.use(express.static(__dirname + "/public"));
+
+
+io.sockets.on("error", e => console.log(e));
+
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
